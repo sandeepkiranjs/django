285d684a2259821c3c533bdd06b294a5857dcefc
sandeepkiranjs <sandeepkiranjs1998@gmail.com>
2020-03-10 15:19:16 +0530

    "changes index html"

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hello</h1>')
+    return HttpResponse('<h1>Hello world</h1>')
